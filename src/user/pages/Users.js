931654a69b5d0493375ea071e0e13ff3d5082794
shovancak <s.hovancak@gmail.com>
2020-06-16 20:13:48 +0200

    Adding UsersList component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 export default function Users() {
-  return <h2>Users works</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image:
+        "https://i.pinimg.com/originals/94/6e/82/946e829a135f68d7a041e3a83b445f55.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 }
