184e2564aa61b3853f91aba7b9437c2455de576c
shovancak <s.hovancak@gmail.com>
2020-06-16 17:25:01 +0200

    Implementig basic routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
