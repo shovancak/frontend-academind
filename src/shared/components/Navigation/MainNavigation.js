4fdeb179c3d9eedbb9c5bf51b995069c18e36faa
shovancak <s.hovancak@gmail.com>
2020-06-18 19:45:19 +0200

    Changing Title in Header

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -36,7 +36,7 @@ const MainNavigation = () => {
           <span />
         </button>
         <h1 className="main-navigation__title">
-          <Link to="/">YourPlaces</Link>
+          <Link to="/">MyPlaces</Link>
         </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
