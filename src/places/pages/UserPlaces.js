62b8e8b2c37c886476d9e751a8cb425fec10c09d
shovancak <s.hovancak@gmail.com>
2020-06-18 17:56:44 +0200

    Setting Places rendering components

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Emipre State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emipre State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://untappedcities.com/wp-content/uploads/2015/07/Flatiron-Building-Secrets-Roof-Basement-Elevator-Sonny-Atis-GFP-NYC_5.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
