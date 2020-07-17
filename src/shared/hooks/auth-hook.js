c5ab2e989b5b5476d87f812ebafcdd1a9ad9a78a
shovancak <s.hovancak@gmail.com>
2020-07-17 20:58:03 +0200

    Minor code changes + refactoring

--- src/shared/hooks/auth-hook.js
+++ src/shared/hooks/auth-hook.js
@@ -1,24 +1,24 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect } from 'react';
 
 let logoutTimer;
 
 export const useAuth = () => {
   const [token, setToken] = useState(false);
-  const [tokenExporationDate, setTokenExpirationDate] = useState();
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
   const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    const tokenExporationTime =
+    const tokenExpirationDate =
       expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(tokenExporationTime);
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
-      "userData",
+      'userData',
       JSON.stringify({
         userId: uid,
         token: token,
-        expiration: tokenExporationTime.toISOString(),
+        expiration: tokenExpirationDate.toISOString()
       })
     );
   }, []);
@@ -27,31 +27,26 @@ export const useAuth = () => {
     setToken(null);
     setTokenExpirationDate(null);
     setUserId(null);
-    localStorage.removeItem("userData");
+    localStorage.removeItem('userData');
   }, []);
 
   useEffect(() => {
-    if (token && tokenExporationDate) {
-      const remainingTime =
-        tokenExporationDate.getTime() - new Date().getTime();
+    if (token && tokenExpirationDate) {
+      const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
       logoutTimer = setTimeout(logout, remainingTime);
     } else {
       clearTimeout(logoutTimer);
     }
-  }, [token, logout, tokenExporationDate]);
+  }, [token, logout, tokenExpirationDate]);
 
   useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
+    const storedData = JSON.parse(localStorage.getItem('userData'));
     if (
       storedData &&
       storedData.token &&
       new Date(storedData.expiration) > new Date()
     ) {
-      login(
-        storedData.userId,
-        storedData.token,
-        new Date(storedData.expiration)
-      );
+      login(storedData.userId, storedData.token, new Date(storedData.expiration));
     }
   }, [login]);
 
